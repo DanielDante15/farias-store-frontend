c94aee096ecca80137133480c60e3109a5680352
DanielDante15 <danieldante>
2023-11-28 20:19:13 -0300

    Co-authored-by: Caio Farias <caiofariaas@users.noreply.github.com>

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,22 +1,36 @@
 import Card from "@/components/card";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 
 function HomeScreen() {
+    const [tenisData, setTenisData] = useState([]);
+
+
+    useEffect(() => {
+        axios
+            .get('http://192.168.1.101:8000/tenis/')
+            .then(response => {
+                setTenisData(response.data); // Atualize o estado com os dados recebidos
+            })
+            .catch(error => {
+                // Lidar com erros aqui
+                console.error('Erro na requisição:', error);
+            });
+    }, []);
+
+
 
     return (<>
         <div className=" flex justify-center bg-[#222] z-0 text-center h-full w-full  ">
 
             <div className="flex flex-wrap w-1/2">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+                {tenisData.map((tenisItem, index) => (
+                    <Card key={index} data={tenisItem} />
+                ))}
+
             </div>
-          
+
 
 
         </div>
